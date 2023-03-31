934ece6e18933c990d4f09e390c12461fd4bc936
Alexander Lazarenko <ferrosful@gmail.com>
2023-03-31 20:45:05 +0300

    Initial Commit.

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
+import path from 'path';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), svgr({ exportAsDefault: true })],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@shared': path.resolve(__dirname, './src/shared'),
+    },
+  },
+  server: {
+    port: 3000,
+  },
+});
