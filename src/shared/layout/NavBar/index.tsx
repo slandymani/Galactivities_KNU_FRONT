fdbac7e53f6aac0c58f23a83bbbac90c81c78f1c
slandymani <slandyman9@gmail.com>
2023-05-27 23:08:23 +0300

    fix env

--- src/shared/layout/NavBar/index.tsx
+++ src/shared/layout/NavBar/index.tsx
@@ -53,9 +53,9 @@ function NavBar() {
           {APP_NAME}
         </Menu.Item>
         <Menu.Item as={NavLink} to={ROUTES.ACTIVITIES.LIST} name="Activities" />
-        <Menu.Item>
-          <Button as={NavLink} to={ROUTES.ACTIVITIES.CREATE} positive content="Create Activity" />
-        </Menu.Item>
+        <Menu.Item
+            as={NavLink} to={ROUTES.ACTIVITIES.CREATE} positive content="Create Activity"
+        />
         <Menu.Item position="right">
           <Image src={user?.imageUri || IMAGE_URIS.USER_DEFAULT} avatar spaced="right" />
           <Dropdown pointing="top left" text={user?.displayName}>
